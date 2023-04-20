47cd83a5f718ba44204902a5ece97b3d37bd58d9
조성훈 <josunghun@joseonghun-ui-MacBookPro.local>
2023-04-20 13:41:00 +0900

    1
    1

---
+++ Sunrint_Ingopass_Backend/src/main/java/com/sunrint/ingopass/sunrint_ingopass_backend/SunrintIngopassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sunrint.ingopass.sunrint_ingopass_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunrintIngopassBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunrintIngopassBackendApplication.class, args);
+	}
+
+}
---
+++ Sunrint_Ingopass_Backend/src/test/java/com/sunrint/ingopass/sunrint_ingopass_backend/SunrintIngopassBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunrint.ingopass.sunrint_ingopass_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunrintIngopassBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
